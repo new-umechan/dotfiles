af35287c2238b5f3be809fee41c10dc749cafcda
new-umechan <ryu.shamoji@gmail.com>
2026-04-18 00:24:59 +0900

    chore: flashのpluginを追加

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -10,6 +10,7 @@ return {
 		},
 	},
 	keys = {
+		{ "<leader>s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
 		{ "+", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
 		{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
 		{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
