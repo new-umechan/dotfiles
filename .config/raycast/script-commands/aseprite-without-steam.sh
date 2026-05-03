3a1010a127d0cea8eda126492139030964d39764
new-umechan <ryu.shamoji@gmail.com>
2026-05-03 22:45:48 +0900

    feat: asepriteをraycastで直で開けるように

---
+++ .config/raycast/script-commands/aseprite-without-steam.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title aseprite without steam
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon ./aseprite.jpg
+
+# Documentation:
+# @raycast.author ume
+
+open "/Volumes/ssd/SteamLibrary/steamapps/common/Aseprite/Aseprite.app"
